6a67985e8c040ae45eb50483e0245d743f3695d9
jw-pyo <wjddn1801@snu.ac.kr>
2017-09-27 17:40:59 +0900

    add skeleton

---
+++ lab_3/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+##### Conversation Service Credentials info #####
+endpoint_url = 'https://gateway.aibril-watson.kr/conversation/api'
+conversation_username = '7436f7f6-86b8-4a10-a207-33b0ad5d6406'
+conversation_password = '1bgVT6ElP0ZN'
+workspace_main = '1e00e7ff-b92b-48d9-8b77-fc4566952cdb'
